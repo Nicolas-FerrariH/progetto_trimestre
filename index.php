57a088567ceec50c50a27c07f4fb806856475eee
Nicolas-FerrariH <nicolas.ferrari.studenti@isii.it>
2022-11-25 08:50:24 +0100

    lavoro mattina

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 require("_dbFunc.php");
 $conn=db_connect();
 
-
-
-
 ?>
 
 <html>
@@ -13,6 +10,9 @@ $conn=db_connect();
         <link rel="stylesheet" href="stile.css">
     </head>
     <body>
-        
+        <div class="nav_index">
+            <a href="index.php">Ambulatorio PICC</a>
+            <a class="dx" href="login.php">Login</a>
+        </div>
     </body>
 </html>
